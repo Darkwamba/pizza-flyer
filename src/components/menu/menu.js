10bf3753d61b9403e14e518c76ca20d835127913
Salvatore Martello <sa.martello@gmail.com>
2021-07-17 17:15:08 +0200

    Menu generato dinamicamente da query graphql

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -4,10 +4,25 @@ import Category from './category'
 import ToggleButton  from './togglebutton'
 import {Typography} from "@material-ui/core"
 
+const getCategoryType= (name)=>{
+  let regex = /pizz/i;
+  if(name.match(regex)!==null)
+    return 'pizza';
+  let regex2 = /bibit|bevand|drink/i;
+  if(name.match(regex2)!==null)
+    return 'drink';
+  else return 'food';
+}
+
+// eslint-disable-next-line no-unused-vars
+const capitalizeFirstLetter = (string)=>{
+  return string[0].toUpperCase() + string.slice(1);
+}
+
 const Menu = () => {
   const localmenu = useStaticQuery(graphql`
   {
-    pizzeclassiche: allDatoCmsPizza(filter: {categoria: {nome: {eq: "classiche"}}}) {
+    pizzeclassiche: allDatoCmsPizza(filter: {categoria: {idcategoria: {eq: 1}}}) {
       edges {
         node {
           idprodotto
@@ -28,7 +43,7 @@ const Menu = () => {
         }
       }
     }
-      pizzegourmet: allDatoCmsPizza(filter: {categoria: {nome: {eq: "gourmet"}}}) {
+    pizzegourmet: allDatoCmsPizza(filter: {categoria: {idcategoria: {eq: 2}}}) {
       edges {
         node {
           idprodotto
@@ -62,6 +77,9 @@ const Menu = () => {
               nome
             }
           }
+          categoria {
+            nome
+          }
         }
       }
     }
@@ -72,39 +90,32 @@ const Menu = () => {
           nome
           prezzoNormale
           order
+          categoria {
+            nome
+          }
         }
       }
     }
   }
   `)
-
-  /*const [menuCategory, setMenuCategory] = useState('hotDrinks');
-  let SelectedCategory;
-  if (menuCategory === 'hotDrinks' ) {
-    SelectedCategory = HotDrinks;
-  } else if (menuCategory === 'coldDrinks') {
-    SelectedCategory = ColdDrinks
-  } else if (menuCategory === 'breakfast') {
-    SelectedCategory = Breakfast
-  } else if (menuCategory === 'lunch') {
-    SelectedCategory = Lunch
-  }
-  */
   return (
     <section className="menu">
-      <Typography variant="h3" component="p">Menu</Typography>
-      <ToggleButton name="Pizze classiche">
-          <Category data={localmenu.pizzeclassiche} type={"Pizza"} />
-      </ToggleButton>
-      <ToggleButton name="Pizze gourmet">
-          <Category data={localmenu.pizzegourmet} type={"Pizza"} />
-      </ToggleButton>
-      <ToggleButton name="Panini">
-          <Category data={localmenu.allDatoCmsPiada}  type={"Food"}  />
-      </ToggleButton>
-      <ToggleButton name="Bibite">
-          <Category data={localmenu.allDatoCmsBevanda}  type={"Drink"} />
-      </ToggleButton>
+      <Typography variant="h3" component="p">
+        Menu
+      </Typography>
+      {Object.values(localmenu).map((categoryData, index) => {
+        console.log(categoryData)
+        let category = "undefined"
+        if ("categoria" in categoryData.edges[0].node)
+          category = categoryData.edges[0].node.categoria.nome
+        let category_name = capitalizeFirstLetter(category)
+        let category_type = getCategoryType(category)
+        return (
+          <ToggleButton key={index} name={category_name}>
+            <Category data={categoryData} type={category_type} />
+          </ToggleButton>
+        )
+      })}
     </section>
   )
 }
